6fd334f8aa6cf4d7bca68f71470375d3b7367330
Aleksey <Lesha55-90@mail.ru>
2023-11-19 19:14:11 +0600

    headless change

--- src/test/java/ru/netology/DC/DeliverTCardTest.java
+++ src/test/java/ru/netology/DC/DeliverTCardTest.java
@@ -2,13 +2,9 @@ package ru.netology.DC;
 
 
 import com.codeborne.selenide.Selenide;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 
 
 import static com.codeborne.selenide.Selenide.$;
@@ -17,26 +13,6 @@ import static com.codeborne.selenide.Selenide.$;
 public class DeliverTCardTest {
 
 
-    private WebDriver driver;
-
-    @BeforeAll
-    public static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
-
-    }
-
-    @BeforeEach
-    public void setup() {
-        driver = new ChromeDriver();
-
-
-    }
-
-    @AfterEach
-    void teardown() {
-        driver.quit();
-
-    }
 
     @Test
     public void shouldTestСardDelivery() {
